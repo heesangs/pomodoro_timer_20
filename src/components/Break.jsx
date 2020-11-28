d74d4656bdfcf17e810a3013ebe5098a8d2411eb
heesangs_mac <heesangs@heesangs-macui-MacBookPro.local>
2020-11-28 19:08:05 +0900

    break-incre/decre_logic_with_Hook

--- src/components/Break.jsx
+++ src/components/Break.jsx
@@ -1,8 +1,28 @@
 import React from 'react';
+import { useState } from 'react';
 
 const Break = () => {
+    const [breakLength, setBreakLength] = useState(300);
+    const incrementBreakLengthByOneMinute = () => {
+        setBreakLength(breakLength + 60);
+    }
+    const decrementBreakLengthByOneMinute = () => {
+        let decrementBreakLength = breakLength - 60
+        if (decrementBreakLength < 0) {
+            setBreakLength(0);
+        } else {
+            setBreakLength(decrementBreakLength);
+        }
+
+
+    }
     return (
-        <p id="break-label">break</p>
+        <div>
+            <p id="break-label">break</p>
+            <p id="break-Length">{breakLength}</p>
+            <button id="break-increment" onClick={incrementBreakLengthByOneMinute}>+</button>
+            <button id="break-decrement" onClick={decrementBreakLengthByOneMinute}>-</button>
+        </div>
     )
 }
 
